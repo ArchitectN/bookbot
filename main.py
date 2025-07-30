4aa9ea9bbfb0f0f77bd57e8058d958a90632d7f9
boot_dev_nick <nick@boot.com>
2025-07-30 16:34:52 -0400

    middle of ch3 l1

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from stats import get_num_words
+from stats import get_char_count
 
 def main():
     path = "books/frankenstein.txt"
     book = get_book_text(path)
     words_count = get_num_words(path)
+    char_count = get_char_count(path)
     print(words_count)
+    print(char_count)
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -7,9 +7,19 @@ def get_num_words(path):
     return f"{total_words} words found in the document"
 
 
-letter_count = {}
+def sort(items):
+    return items["num"]
 
 def get_char_count(path):
+    letter_count = {}
     with open(path) as file:
         content = file.read()
-        words = 
+        content_lower = content.lower()
+        char = list(content_lower)
+        for item in char:
+            if item not in letter_count:
+                letter_count[item] = 1
+            else:
+                letter_count[item] += 1
+    letter_count.sort(reverse=True, key=sort_on)
+    return letter_count
